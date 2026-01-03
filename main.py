ed7002eca4b3ad52a4eb18aa0ce191e2b2c2ae12
unknown <mg.mobile9091@gmial.com>
2026-01-03 16:03:21 +0330

    Small update for my pass checker test.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-# برنامه تشخیص قبولی
+# student pass checker
 
 name = input("Enter student name: ")
 score = float(input("Enter score (0-20): "))
 
-if score >= 10:
-    print(f"{name} passed the exam!")
-else:
-    print(f"{name} failed the exam!")
+if score < 0 or score > 20 :
+    print('invalid score! ')
+
+elif score == 10 :
+    print(f"{name} passed (border line).")
+
+elif score > 10 :
+    print(f'{name} passed.')
+
+elif score < 10 :
+    print(f'{name} failed.')
+
